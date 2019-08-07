72597e4f95c7bea3c98af81e0c44cf08b69d6e37
vikymore541 <vikymore541@gmail.com>
2019-08-07 22:49:30 +0530

    my first todo app in angular

---
+++ src/app/app.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'todo-app';
+    todoArray=[]
+    addTodo(value){ 
+      this.todoArray.push(value)
+       console.log(this.todoArray);
+    }
+    /*delete item*/  deleteItem(todo){   
+      for(let i=0 ;i<= this.todoArray.length ;i++){  
+          if(todo== this.todoArray[i]){     
+            this.todoArray.splice(i,1)   
+           }  
+           } 
+           }
+
+}
